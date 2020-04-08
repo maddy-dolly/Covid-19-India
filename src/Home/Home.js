0b2c16375f6e1d27ef589cc0641ee129dc70a37d
maddy <maddy-dolly>
2020-04-08 22:18:08 +0530

    second-map.js

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,6 +2,7 @@
 import React, {Component}  from 'react';
 import { Sparklines, SparklinesCurve, SparklinesSpots } from 'react-sparklines';
 import './Home.css'
+import axios from 'axios';
 import Map from '../MapHome/Map';
 
 
@@ -28,10 +29,69 @@ class Home extends Component {
       count: null,
       total: [],         
       stateitems: [],
-      selectedState:[]
+      item: [],
+      selectedState: [],
+      max: 0,
+      counter:0
     };
+    this.check = this.checkCounter.bind(this)
   }
+  mapCoponentApi() {
+    axios.get("https://api.covid19india.org/data.json")
+    .then(
+      result => {
+        const statewise = result.data.statewise;
+        if(this.state.selectedState.length === 0) {
+          const total = result.data.statewise.filter((item) => {
+              return (item.state === 'Total');
+            });
+            this.setState({
+                selectedState: total[0]
+            })
+          var max = 0;
+          for(var i=0;i<result.data.statewise.length;i++) {
+              if(result.data.statewise[i].state !== 'Total') {
+                  if(+result.data.statewise[i].confirmed >= max) {
+                      max = +result.data.statewise[i].confirmed;
+                  }
+              }
+          }
+          max = Math.floor(max/5);
+          this.setState({
+              max: max
+          })
+          console.log('hyy');
+          
+        }
+        this.setState({
+          isLoaded: true,
+          items: statewise,
+          max: max
+        });
+      },
+      error => {
+        this.setState({
+          isLoaded: true,
+          error: error
+        });
+      }
+    );
+  }
+  getLocationName(event) {
+    return event.target.attributes.id.value;
+}
 
+  checkCounter(event) {
+    const hovered = this.getLocationName(event).toUpperCase();
+    // console.log('Before Chnage',this.state.selectedState);
+    const total = this.state.items.filter((item) => {
+        return (item.statecode === hovered);
+    });
+    this.setState({
+        selectedState: total[0],
+        
+    });
+  }
 
   componentDidMount(){
     fetch('https://api.covid19india.org/data.json')
@@ -61,149 +121,151 @@ class Home extends Component {
         activeCase.push((+case_time_data[i].totalconfirmed)-((+case_time_data[i].totalrecovered)+(+case_time_data[i].totaldeceased)))
       }
       this.setState({
-          showGraph: true,
+          isLoaded: true,
       })
     });
+    this.mapCoponentApi();
   }
 
 
 
   render() {   
        
-      if(! this.state.showGraph) {
+      if(this.state.isLoaded) {
         return(
-            <div className="IndiaData">
-                <div className="col-md-4">
-                Loading......
-                </div>            
-                <div className="col-md-8">
-                    Loading......
-                </div>
-            </div>
-        );
-      }
-      else {
-            return(
 
-                <div className="card">
-                  <div className="card-body">
-                  <div className="row">
-                    <div className="col-md-5">
-                    <div className="row">
-                            <div className="col-md-8">
-                              <h3 className="main_heading">INDIA COVID-19 TRACKER</h3>
-                              <p className="main_para">A CROWDSOURCED INITIATIVE</p>
-                            </div>
-                            <div className="col-md-4">
-                              <p className="para_one">LAST UPDATE</p>
-                              <p className="span_last_time">{lastupdatedtime}</p>
-                            </div>
-                          </div>
-                        <div className="row">
-                         
-                            <div className="col-md-3" style={{ color:'#ff073a99',textAlign:'center'}}>
-                                <h6>Confirmed</h6>
-                                <h5 className="heading_h5_style">[+{totalDeltaConfirmed}]</h5>
-                                <h3 style={{color:'red'}}>{totalConfirmed}</h3>
-                                <Sparklines data={confirmedCase} svgWidth={100} >
-                                    <SparklinesCurve style={{ strokeWidth: 5, stroke: "#ff073a99", fill: "none" }} />
-                                    <SparklinesSpots size={7} />
-                                </Sparklines>
-                            </div>
-                            <div className="col-md-3" style={{ color: 'rgba(0,123,255,.6)',textAlign:'center'}}>
-                                <h6>Active</h6>
-                                <h5>&nbsp;</h5>
-                                <h3 style={{color:'blue'}}>{totalActive}</h3>
-                                <Sparklines data={activeCase} svgWidth={100} limit={30}>
-                                    <SparklinesCurve style={{ strokeWidth: 5, stroke: "rgba(0,123,255,.6)", fill: "none" }} />
-                                    <SparklinesSpots size={7}/>
-                                </Sparklines>
-                            </div>
-                            <div className="col-md-3" style={{color: 'rgba(40,167,69,.6)',textAlign:'center'}}>
-                                <h6>Recovered</h6>
-                                <h5 className="heading_h5_style">[+{totalDeltaRecovered}]</h5>
-                                <h3 style={{color:'green'}}>{totalRecovered}</h3>
-                                <Sparklines data={recoveredCase} limit={20} svgWidth={100} >
-                                    <SparklinesCurve style={{ strokeWidth: 5, stroke: "rgba(40,167,69,.6)", fill: "none" }} />
-                                    <SparklinesSpots size={7}/>
-                                </Sparklines>
-                            </div>
-                            <div className="col-md-3" style={{ color: 'rgba(108,117,125,.6)',textAlign:'center'}}>
-                                <h6>Deceased</h6>
-                                <h5 className="heading_h5_style">[+{totalDeltaDeaths}]</h5>
-                                <h3 style={{color:'gray'}}>{totalDeceased}</h3>
-                                <Sparklines data={deceasesCase} limit={10} svgWidth={100} >
-                                    <SparklinesCurve style={{ strokeWidth: 5, stroke: "rgba(108,117,125,.6)", fill: "none" }} />
-                                    <SparklinesSpots size={7}/>
-                                </Sparklines>
-                            </div>
-                        </div>
-                        </div>
-                        <div className="col-md-1"></div>
-                                   
-                    <div className="col-md-6">
-                      <div>
-                        <h6 className="main_heading">India</h6>
-                        <p className="main_para">HOVER OVER A STATE/UT FOR MORE DETAILS</p>
+          <div className="card">
+            <div className="card-body">
+            <div className="row">
+              <div className="col-md-5">
+              <div className="row">
+                      <div className="col-md-8">
+                        <h3 className="main_heading">INDIA COVID-19 TRACKER</h3>
+                        <p className="main_para">A CROWDSOURCED INITIATIVE</p>
                       </div>
-                          <div className="row">
-                            <div className="col-md-3">
-                              <div className="card_one">
-                               
-                                  <h6 className="heading_6_style" style={{color:'#ef6060'}}>Confirmed</h6>
-                                  <div className="stats-bottom">
-                                       <h1 className="heading_1_count_style">{totalConfirmed}</h1>
-                                    </div>
-                                
-                              </div>
-                            </div>
-                            <div className="col-md-3">
-                            <div className="card_two">
-                               
-                            <h6 className="heading_6_style" style={{color:'rgba(0,123,255,.6)'}}>Active</h6>
-                                  <div className="stats-bottom">
-                                       <h1 className="heading_1_count_style" style={{color:'#007bff'}}>{totalActive}</h1>
-                                    </div>
-                             
-                           </div>
-                            </div>
-                            <div className="col-md-3">
-                            <div className="card_three">
-                               
-                            <h6 className="heading_6_style" style={{color:'rgba(40,167,69,.6)'}}>Recovered</h6>
-                            <div className="stats-bottom">
-                                  <h1 className="heading_1_count_style" style={{color:'#28a745'}}>{totalRecovered}</h1>
-                            </div>
-                             
-                           </div>
-                            </div>
-                            <div className="col-md-3">
-                            <div className="card_four">
-                               
-                            <h6 className="heading_6_style" style={{color:'rgba(108,117,125,.6)'}}>Deceased</h6>
-                              <div className="stats-bottom">
-                                    <h1 className="heading_1_count_style" style={{color:'#6c757d'}}>{totalDeltaDeaths}</h1>
-                              </div>
-                             
-                           </div>
-                            </div>
-                          </div>
-                          <div className="row">
-                            <div className="col-md-8">
-                              <h3 className="country_heading">India</h3>
-                            </div>
-                            <div className="col-md-4" style={{textAlign: 'right',marginLeft: '-45px'}}>
-                              <p className="para_onee">LAST UPDATE</p>
-                              <p className="span_last_timee">{lastupdatedtime}</p>
-                            </div>
-                          </div>
+                      <div className="col-md-4">
+                        <p className="para_one">LAST UPDATE</p>
+                        <p className="span_last_time">{lastupdatedtime}</p>
+                      </div>
                     </div>
+                  <div className="row">
+                   
+                      <div className="col-md-3" style={{ color:'#ff073a99',textAlign:'center'}}>
+                          <h6>Confirmed</h6>
+                          <h5 className="heading_h5_style">[+{totalDeltaConfirmed}]</h5>
+                          <h3 style={{color:'red'}}>{totalConfirmed}</h3>
+                          <Sparklines data={confirmedCase} svgWidth={100} >
+                              <SparklinesCurve style={{ strokeWidth: 5, stroke: "#ff073a99", fill: "none" }} />
+                              <SparklinesSpots size={7} />
+                          </Sparklines>
+                      </div>
+                      <div className="col-md-3" style={{ color: 'rgba(0,123,255,.6)',textAlign:'center'}}>
+                          <h6>Active</h6>
+                          <h5>&nbsp;</h5>
+                          <h3 style={{color:'blue'}}>{totalActive}</h3>
+                          <Sparklines data={activeCase} svgWidth={100} limit={30}>
+                              <SparklinesCurve style={{ strokeWidth: 5, stroke: "rgba(0,123,255,.6)", fill: "none" }} />
+                              <SparklinesSpots size={7}/>
+                          </Sparklines>
+                      </div>
+                      <div className="col-md-3" style={{color: 'rgba(40,167,69,.6)',textAlign:'center'}}>
+                          <h6>Recovered</h6>
+                          <h5 className="heading_h5_style">[+{totalDeltaRecovered}]</h5>
+                          <h3 style={{color:'green'}}>{totalRecovered}</h3>
+                          <Sparklines data={recoveredCase} limit={20} svgWidth={100} >
+                              <SparklinesCurve style={{ strokeWidth: 5, stroke: "rgba(40,167,69,.6)", fill: "none" }} />
+                              <SparklinesSpots size={7}/>
+                          </Sparklines>
+                      </div>
+                      <div className="col-md-3" style={{ color: 'rgba(108,117,125,.6)',textAlign:'center'}}>
+                          <h6>Deceased</h6>
+                          <h5 className="heading_h5_style">[+{totalDeltaDeaths}]</h5>
+                          <h3 style={{color:'gray'}}>{totalDeceased}</h3>
+                          <Sparklines data={deceasesCase} limit={10} svgWidth={100} >
+                              <SparklinesCurve style={{ strokeWidth: 5, stroke: "rgba(108,117,125,.6)", fill: "none" }} />
+                              <SparklinesSpots size={7}/>
+                          </Sparklines>
+                      </div>
+                  </div>
+                  </div>
+                  <div className="col-md-1"></div>
+                             
+              <div className="col-md-6">
+                <div>
+                  <h6 className="main_heading">India</h6>
+                  <p className="main_para">HOVER OVER A STATE/UT FOR MORE DETAILS</p>
                 </div>
+                    <div className="row">
+                      <div className="col-md-3">
+                        <div className="card_one">
+                         
+                            <h6 className="heading_6_style" style={{color:'#ef6060'}}>Confirmed</h6>
+                            <div className="stats-bottom">
+                                 <h1 className="heading_1_count_style">{this.state.selectedState.confirmed}</h1>
+                              </div>
+                          
+                        </div>
+                      </div>
+                      <div className="col-md-3">
+                      <div className="card_two">
+                         
+                      <h6 className="heading_6_style" style={{color:'rgba(0,123,255,.6)'}}>Active</h6>
+                            <div className="stats-bottom">
+                                 <h1 className="heading_1_count_style" style={{color:'#007bff'}}>{this.state.selectedState.active}</h1>
+                              </div>
+                       
+                     </div>
+                      </div>
+                      <div className="col-md-3">
+                      <div className="card_three">
+                         
+                      <h6 className="heading_6_style" style={{color:'rgba(40,167,69,.6)'}}>Recovered</h6>
+                      <div className="stats-bottom">
+                            <h1 className="heading_1_count_style" style={{color:'#28a745'}}>{this.state.selectedState.recovered}</h1>
+                      </div>
+                       
+                     </div>
+                      </div>
+                      <div className="col-md-3">
+                      <div className="card_four">
+                         
+                      <h6 className="heading_6_style" style={{color:'rgba(108,117,125,.6)'}}>Deceased</h6>
+                        <div className="stats-bottom">
+                              <h1 className="heading_1_count_style" style={{color:'#6c757d'}}>{this.state.selectedState.deaths}</h1>
+                        </div>
+                       
+                     </div>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-8">
+                        <h3 className="country_heading">{this.state.selectedState.state.toUpperCase()}</h3>
+                      </div>
+                      <div className="col-md-4" style={{textAlign: 'right',marginLeft: '-45px'}}>
+                        <p className="para_onee">LAST UPDATE</p>
+                        <p className="span_last_timee">{lastupdatedtime}</p>
+                      </div>
+                    </div>
               </div>
-              <Map></Map>
+          </div>
+        </div>
+        <Map check={this.check}></Map>
+        </div>
+      );
+  }
+        
+      else {
+        return(
+          <div className="IndiaData">
+              <div className="col-md-4">
+              Loading......
+              </div>            
+              <div className="col-md-8">
+                  Loading......
               </div>
-            );
-        }
+          </div>
+      );
+    }   
   }
 }
 export default Home;
--- src/MapHome/Map.js
+++ src/MapHome/Map.js
@@ -22,7 +22,7 @@ class Map extends Component  {
             isLoaded: false,
             items: [],
             selectedState: [],
-            max: 0
+            max: 0,
           };
 
         this.onHoverHandler = this.onHoverHandler.bind(this);
@@ -31,8 +31,7 @@ class Map extends Component  {
             ...India,
             label: "Custom map label",
             locations: India.locations.map(location => {
-              // Modify each location
-            //   console.log('Location is',location);              
+                         
             })
           };
     }
@@ -78,21 +77,9 @@ class Map extends Component  {
     }
 
 
-    getLocationName(event) {
-        return event.target.attributes.id.value;
-    }
-
+  
     onHoverHandler (event) {
-        const hovered = this.getLocationName(event).toUpperCase();
-        // console.log('Before Chnage',this.state.selectedState);
-        const total = this.state.items.filter((item) => {
-            return (item.statecode === hovered);
-        });
-        this.setState({
-            selectedState: total[0]
-        });
-        // console.log('After Chnage',this.state.selectedState);     
-
+     this.props.check(event)
         
     }
 
@@ -131,6 +118,7 @@ class Map extends Component  {
         if(this.state.isLoaded) {
             return (
                 <div className="MapData" style={{width: '80%', margin: 'auto'}}>
+                    <div className="col-md-6"></div>
                     <div className="col-lg-6" style={{height: '50%'}}>
                         <SVGMap 
                             map={India} 
@@ -138,37 +126,7 @@ class Map extends Component  {
                             locationClassName={this.getLocationClassName}
                         />
                     </div>
-                    <div className="col-lg-6">
-                        <div className="MapData">
-                            <span className="col-lg-6" style={{textAlign:'left',paddingLeft: 20, color: '#ff0739', fontWeight: 'bold'}}>
-                                {this.state.selectedState.state.toUpperCase()}
-                            </span>
-                            <span className="col-lg-6" style={{textAlign:'right',paddingRight: 20, color: '#28a744'}}>
-                                    <span style={{fontSize: 13}}>LAST UPDATED</span>
-                                    <h6>{timeAgo.format(new Date())}</h6>
-                            </span>
-                        </div>
-                        <div className="col-lg-12" style={{marginBottom: 20, color:'#6f42c1'}}>
-                            <h6>Confirmed</h6>
-                            <h5>[+{this.state.selectedState.deltaconfirmed}]</h5>
-                            <h3>{this.state.selectedState.confirmed}</h3>
-                        </div>
-                        <div className="col-lg-12" style={{marginBottom: 20, color:'#6b757d'}}>
-                            <h6>Active</h6>
-                            <h5>[+{this.state.selectedState.active}]</h5>
-                            <h3>{this.state.selectedState.active}</h3>
-                        </div>
-                        <div className="col-lg-12" style={{marginBottom: 20, color:'#28a744'}}>
-                            <h6>Recovered</h6>
-                            <h5>[+{this.state.selectedState.deltarecovered}]</h5>
-                            <h3>{this.state.selectedState.recovered}</h3>
-                        </div>
-                        <div className="col-lg-12" style={{marginBottom: 20, color:'#ff0739'}}>
-                            <h6>Deceased</h6>
-                            <h5>[+{this.state.selectedState.deltadeaths}]</h5>
-                            <h3>{this.state.selectedState.deaths}</h3>
-                        </div>
-                    </div>
+                    
                 </div>
             );
         }
